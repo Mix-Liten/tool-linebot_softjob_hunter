88ff32b9b6338c2cfa352e0c6af8831846ff1197
Mix-Liten <mixliten@gmail.com>
2019-11-27 11:04:29 +0800

    update action crawler - fix and add dataBaseInit func

--- action/crawler.js
+++ action/crawler.js
@@ -2,16 +2,16 @@ const request = require('request')
 const cheerio = require('cheerio')
 
 const getJobs = async () => {
-  const indexURL = process.env.PTT_SOFTJOB_URL.replace(/<pageNumber>/, '1525') // 最新的頁面沒有 pageNumber。
+  const indexURL = process.env.PTT_SOFTJOB_URL.replace(/<pageNumber>/, '') // 最新的頁面沒有 pageNumber
   let result = []
   let jobslist = await getJobsFormCurrentPage(indexURL)
-  if (jobslist === null) return null
 
   do {
-    nextURL = process.env.PTT_SOFTJOB_URL.replace(/<pageNumber>/, jobslist.prevpageNumber)
     result = [...result, ...jobslist.list]
+    if (jobslist.prevpageNumber === null) break
+    nextURL = process.env.PTT_SOFTJOB_URL.replace(/<pageNumber>/, jobslist.prevpageNumber)
     jobslist = await getJobsFormCurrentPage(nextURL)
-  } while (!jobslist.hasOtherDate && jobslist === null)
+  } while (!jobslist.hasOtherDate)
   return result
 }
 
@@ -20,26 +20,26 @@ const getJobsFormCurrentPage = async (url) => {
     request(url, async (err, res, body) => {
       if (err) return reject(err)
 
-      const prevpageNumber = await getPttFirstPrevpageNumber(body) // 取得「上頁」按鈕的 href 中 index 到 .html 中間的數值
+      const prevpageNumber = await getPttFirstPrevpageNumber(body)
       const jobslist = await getPttJobslist(body)
-      if (jobslist === null) return resolve(null)
 
       const regex = /(.*)\/(.*)/
       const date = new Date()
       const today = [date.getMonth() + 1, date.getDate()]
       const datelist = jobslist.map(job => job.date.match(regex))
 
-      let result = {}
       let hasOtherDate = false
       datelist.forEach(date => {
+        // date[] => [ '11/21', '11', '21', index: 0, input: '11/21', groups: undefined ]
         if (date[1] !== today[0] || date[2] !== today[1]) {
           hasOtherDate = true
         }
       })
-      result.hasOtherDate = hasOtherDate
-      result.prevpageNumber = hasOtherDate ? prevpageNumber : null
-      // result.list = jobslist.filter(job => job.date === `${date.getMonth() + 1}/${date.getDate()}`)
-      result.list = jobslist
+      let result = {
+        hasOtherDate,
+      }
+      result.prevpageNumber = hasOtherDate ? null : prevpageNumber
+      result.list = jobslist.filter(job => job.date === `${today[0]}/${today[1]}`)
       resolve(result)
     })
   })
@@ -52,17 +52,16 @@ const getJobsFormCurrentPage = async (url) => {
 const getPttFirstPrevpageNumber = body => {
   const $ = cheerio.load(body)
   const regex = /index(.*\d)\.html/
-  const href = $('.action-bar')
+  const href = $('.btn-group-paging')
     .find('a')
-    .eq(3)
+    .eq(1)
     .attr('href')
   const prevpageNumber = regex.exec(href)[1]
   return prevpageNumber
 }
 
 /**
- * 取得當前頁面上 title 包含 [徵才] 字串的 title 、 href 、 日期 與 推文數
- * 存到 Database 裡面。
+ * 取得當前頁面上 title 包含 [徵才] 字串的 title、href、日期
  */
 const getPttJobslist = body => {
   const $ = cheerio.load(body)
@@ -78,18 +77,63 @@ const getPttJobslist = body => {
         .parent()
         .parent()
       const ary = regex_company_and_job.exec(title) // 當不成功時會返回 null
-      const company = ary ? ary[1].replace(/誠/, '').trim() : null // 去掉 regex 無法避免的「誠」字
-      const job = ary ? ary[2].trim() : null
+      const company = ary ? ary[1].replace(/誠/, '').trim() : '' // 去掉 regex 無法避免的「誠」字
+      const position = ary ? ary[2].trim() : ''
       const date = r_ent
         .find('.meta .date')
         .text()
         .trim()
-      result.push({ title, company, job, date, href })
+      result.push({ title, company, position, date, href })
     }
   })
-  return result.length !== 0 ? result : null
-};
+  return result
+}
+
+/**
+ * 專案起始執行，直到指定日期(10/20)為止，一直往前翻頁
+ */
+const dataBaseInit = async () => {
+  const getJobsFormCurrentPage = async (url) => {
+    let result = await new Promise((resolve, reject) => {
+      request(url, async (err, res, body) => {
+        if (err) return reject(err)
+  
+        const prevpageNumber = await getPttFirstPrevpageNumber(body)
+        const jobslist = await getPttJobslist(body)
+  
+        const regex = /(.*)\/(.*)/
+        const datelist = jobslist.map(job => job.date.match(regex))
+  
+        let shouldStop = false
+        datelist.forEach(date => {
+          if (date[1] == '10' && parseInt(date[2]) < 20) {
+            shouldStop = true
+          }
+        })
+        let result = {
+          shouldStop,
+          prevpageNumber,
+          list: [...jobslist]
+        }
+        resolve(result)
+      })
+    })
+    return result
+  }
+  const indexURL = process.env.PTT_SOFTJOB_URL.replace(/<pageNumber>/, '')
+  let result = []
+  let jobslist = await getJobsFormCurrentPage(indexURL)
+  if (jobslist === null) return null
+
+  do {
+    result = [...result, ...jobslist.list]
+    nextURL = process.env.PTT_SOFTJOB_URL.replace(/<pageNumber>/, jobslist.prevpageNumber)
+    jobslist = await getJobsFormCurrentPage(nextURL)
+  } while (!jobslist.shouldStop)
+  return result
+}
 
 module.exports = {
   getJobs,
+  dataBaseInit,
 }
