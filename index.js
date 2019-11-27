6f98b626cd58ec9b343f607f2fee7ddd73d56f5b
Mix-Liten <mixliten@gmail.com>
2019-11-27 10:57:38 +0800

    update server index - use autoSchedule

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const router = require('./routes')
+const request = require('request')
+const schedule = require('node-schedule')
 
 app.use(async (req, res, next) => {
   let start = new Date()
   await next()
   let ms = new Date() - start
-  console.log(req.method, req.url, ms)
+  console.log(req.method, req.url, `${ms} ms`)
 })
 
 app.on('error', err => console.log(`server error ${err}`))
@@ -18,3 +20,9 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>
   console.log(`server open on PORT ${PORT}`)
 )
+
+// 每天24點時抓取前一天的職缺
+let autoSchedule = schedule.scheduleJob('0 0 23 * * *', function () {
+  const URL = process.env.SCHEDULE_URL || 'http://localhost:5000/job'
+  request.post(URL)
+})
