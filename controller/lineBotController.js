95172bad3495b0d4dd14358ad15678d68b175b7c
Mix-Liten <mixliten@gmail.com>
2019-11-29 22:56:49 +0800

    fix linebot message process

--- controller/lineBotController.js
+++ controller/lineBotController.js
@@ -5,44 +5,43 @@ const lineCommand = require('../action/db/lineCommand')
 const styleDataToMessage = dbData => {
   let msg = ''
   let num = 1
+  let isMoreThanTwenty = dbData.length > 20
   dbData.forEach(data => {
-    msg += `
-    ${num++}. ${data.title} %0D%0A
-    ${process.env.BASEURL}${data.href}
-    `
+    msg += `${num++}. ${data.date} ${data.title}\n${process.env.BASEURL}${data.href}\n`
   })
+  if (isMoreThanTwenty) msg += helperList[3].message // 避免回復過長，最多回應20筆資料
   return msg
 }
 
 bot.on('message', async (evt) => {
   if (evt.message.type !== 'text') return
   const message = evt.message.text.trim()
-  const cmd = commandList.filter(command => message.indexOf(command['word']) > -1)[0] || []
+  const cmd = commandList.filter(command => message.indexOf(command['word']) > -1)[0] || {}
   const searchWord = message.replace(cmd.regex, '')
   let replyMessage = ''
 
-  if (!cmd.length) {
+  if (!cmd['word']) {
     if (message === '!指令') {
       replyMessage = helperList[1].message // 指令表
     } else {
       replyMessage = helperList[0].message // 非查詢語句的其他回應
     }
-  }
-
-  let data
-  switch (cmd['regex']) {
-    case null:
-      data = await lineCommand(cmd['search'])
-      break
-
-    default:
-      data = await lineCommand(cmd['search'], searchWord)
-      break
-  }
-  if (!data) {
-    replyMessage = helperList[2].message // 查無資料
   } else {
-    replyMessage = styleDataToMessage(data)
+    let data
+    switch (cmd['regex']) {
+      case null:
+        data = await lineCommand(cmd['search'])
+        break
+  
+      default:
+        data = await lineCommand(cmd['search'], searchWord)
+        break
+    }
+    if (data.length) {
+      replyMessage = styleDataToMessage(data)
+    } else {
+      replyMessage = helperList[2].message // 查無資料
+    }
   }
 
   evt.reply(replyMessage)
@@ -52,3 +51,38 @@ bot.on('message', async (evt) => {
 })
 
 module.exports = bot.parser()
+
+// postman testing
+// const test = async (req, res) => {
+//   const message = req.query.message
+//   const cmd = commandList.filter(command => message.indexOf(command['word']) > -1)[0] || {}
+//   const searchWord = message.replace(cmd.regex, '')
+//   let replyMessage = ''
+
+//   if (!cmd['word']) {
+//     if (message === '!指令') {
+//       replyMessage = helperList[1].message // 指令表
+//     } else {
+//       replyMessage = helperList[0].message // 非查詢語句的其他回應
+//     }
+//   } else {
+//     let data
+//     switch (cmd['regex']) {
+//       case null:
+//         data = await lineCommand(cmd['search'])
+//         break
+  
+//       default:
+//         data = await lineCommand(cmd['search'], searchWord)
+//         break
+//     }
+//     if (data.length) {
+//       replyMessage = styleDataToMessage(data)
+//     } else {
+//       replyMessage = helperList[2].message // 查無資料
+//     }
+//   }
+
+//   res.end(replyMessage)
+// }
+// module.exports = test
