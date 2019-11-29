7cf579b92ae92beff17728e580bf253ba1237a5a
Mix-Liten <mixliten@gmail.com>
2019-11-29 23:35:48 +0800

    update action lineCommand find sortBy Id

--- action/db/lineCommand.js
+++ action/db/lineCommand.js
@@ -5,7 +5,7 @@ const defaultWork = async () => {
   return Job
   .find()
   .limit(10)
-  .sort({ createdAt: -1 })
+  .sort({ Id: 1 })
   .select('title date href')
 }
 
@@ -17,19 +17,19 @@ const search = async (searchField, word = '') => {
   } else {
     switch (searchField) {
       case 'in_month':
-        data = await Job.find({ in_month: true }).limit(21).sort({ createdAt: -1 }).select(selectField)
+        data = await Job.find({ in_month: true }).limit(21).sort({ Id: 1 }).select(selectField)
         break
   
       case 'company':
-        data = await Job.find({ company: { $regex: word, $options: 'i' } }).limit(21).sort({ createdAt: -1 }).select(selectField)
+        data = await Job.find({ company: { $regex: word, $options: 'i' } }).limit(21).sort({ Id: 1 }).select(selectField)
         break
   
       case 'position':
-        data = await Job.find({ position: { $regex: word, $options: 'i' } }).limit(21).sort({ createdAt: -1 }).select(selectField)
+        data = await Job.find({ position: { $regex: word, $options: 'i' } }).limit(21).sort({ Id: 1 }).select(selectField)
         break
   
       case 'date':
-        data = await Job.find({ date: word }).limit(21).sort({ createdAt: -1 }).select(selectField)
+        data = await Job.find({ date: word }).limit(21).sort({ Id: 1 }).select(selectField)
         break
   
       default:
